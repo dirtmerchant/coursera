b1a67c7f822c2d2c4f66f03eb9520fa6fd0e43ba
Dirtmerchant <bert@bertbullough.com>
2020-10-11 21:12:53 -0600

    Week2

---
+++ week2/module2Graded.py
@@ -0,0 +1,54 @@
+def color_translator(color):
+	if color == "red":
+		hex_color = "#ff0000"
+	elif color == "green":
+		hex_color = "#00ff00"
+	elif color == "blue":
+		hex_color = "#0000ff"
+    else:
+		hex_color = "unknown"
+	return hex_color
+
+print(color_translator("blue")) # Should be #0000ff
+print(color_translator("yellow")) # Should be unknown
+print(color_translator("red")) # Should be #ff0000
+print(color_translator("black")) # Should be unknown
+print(color_translator("green")) # Should be #00ff00
+print(color_translator("")) # Should be unknown
+
+def exam_grade(score):
+	if score > 95:
+		grade = "Top Score"
+	elif score >= 60:
+		grade = "Pass"
+	else:
+		grade = "Fail"
+	return grade
+
+print(exam_grade(65)) # Should be Pass
+print(exam_grade(55)) # Should be Fail
+print(exam_grade(60)) # Should be Pass
+print(exam_grade(95)) # Should be Pass
+print(exam_grade(100)) # Should be Top Score
+print(exam_grade(0)) # Should be Fail
+
+
+def format_name(first_name, last_name):
+  if first_name != '' and last_name !='':
+     return ("Name: " + first_name + ", " + last_name)
+  elif first_name != ' ' or last_name !=' ':
+     return ("Name: " + first_name + last_name)
+  else:
+     return ''
+
+print(format_name("Ernest", "Hemingway"))
+# Should return the string "Name: Hemingway, Ernest"
+
+print(format_name("", "Madonna"))
+# Should return the string "Name: Madonna"
+
+print(format_name("Voltaire", ""))
+# Should return the string "Name: Voltaire"
+
+print(format_name("", ""))
+# Should return an empty string
