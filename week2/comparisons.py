fce5987712b66adcd6bdefe916f95a0577fb0cb0
Dirtmerchant <bert@bertbullough.com>
2020-10-11 19:34:42 -0600

    comparisons

--- week2/comparisons.py
+++ week2/comparisons.py
@@ -1,3 +1,19 @@
 def hint_username(username):
     if len(username) < 3:
         print("Invalid username. Must be at least 3 characters long.")
+
+def is_positive(number):
+  if number > 0:
+    return True
+
+def number_group(number):
+  if number > 0:
+    return "Positive"
+  elif number < 0:
+    return "Negative"
+  else:
+    return "Zero"
+
+print(number_group(10)) #Should be Positive
+print(number_group(0)) #Should be Zero
+print(number_group(-5)) #Should be Negative
