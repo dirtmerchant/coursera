a173dc7506a8b7ffc7bf75eadeae96b2f43c2ad0
Dirtmerchant <bert@bertbullough.com>
2020-10-18 20:04:24 -0600

    forLoops

---
+++ week3/forLoopsQuiz.py
@@ -0,0 +1,33 @@
+#Fill in the blanks to make the factorial function return the factorial of n. Then, print the first 10 factorials (from 0 to 9) with the corresponding number. Remember that the factorial of a number is defined as the product of an integer and all integers before it. For example, the factorial of five (5!) is equal to 1*2*3*4*5=120. Also recall that the factorial of zero (0!) is equal to 1.
+
+def factorial(n):
+    result = 1
+    for x in range(1,n+1):
+        result = result * x
+    return result
+
+for n in range(0,10):
+    print(n, factorial(n))
+
+# Write a script that prints the first 10 cube numbers (x**3), starting with x=1 and ending with x=10.
+
+for i in range(1,10):
+  print(i**3)
+
+# Write a script that prints the multiples of 7 between 0 and 100. Print one multiple per line and avoid printing any numbers that aren't multiples of 7. Remember that 0 is also a multiple of 7.
+
+x = range(0,100)
+for i in x:
+    if i%7==0:
+        print(i)
+
+def retry(operation, attempts):
+  for n in range(attempts):
+    if operation():
+      print("Attempt " + str(n) + " succeeded")
+      break
+    else:
+      print("Attempt " + str(n) + " failed")
+
+retry(create_user, 3)
+retry(stop_service, 5)
